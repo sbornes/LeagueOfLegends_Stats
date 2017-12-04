86ae3a809bc952762025dfc7ce3c82f8be84d00f
Phillip Truong <philliptruong1@hotmail.com>
2017-12-04 16:38:01 +1100

    tidied up player-info row

--- player.php
+++ player.php
@@ -20,6 +20,8 @@
 	<!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- FontAwesome Glyphicon -->
+  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
   <!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
@@ -69,7 +71,7 @@
 
   <section id="Info">
   	<div class="container">
-  		<div class="mt-5 pt-5 px-3 bg-mydark player-row row">
+  		<div class="mt-5 pt-2 pb-2 px-3 bg-mydark player-row row">
         <div class="col-md-12 col-lg-5">
     			<div class="player-icon d-lg-inline-block mx-auto">
     				<img class="rounded-circle" src="<?php echo $profileIcon; ?>">
@@ -78,17 +80,24 @@
               strtolower($info->rank->flex->tier) : 'unranked' ); ?>-border">
             </div>
     			</div>
-    			<p class="player-name pl-lg-5 mt-4 mt-lg-0 d-lg-inline-block text-center"> <?php echo $info->summoner->name; ?> <span class="badge badge-primary"><?php echo $info->summoner->summonerLevel; ?></span> </p>
+    			<div class="player-name pl-lg-5 mt-4 mt-lg-0 mb-4 d-lg-inline-block">
+            <p class="text-center mb-0"><?php echo $info->summoner->name; ?> <span class="badge badge-primary"><?php echo $info->summoner->summonerLevel; ?></span></p>
+            <div class="player-region d-none d-lg-block text-muted mb-0">
+              <small>#OCEANIA</small>
+            </div>
+          </div>
+
         </div>
+
         <?php if(isset($info->rank)) : ?>
         <div class="col-md-12 col-lg-7">
           <div class="player-rank mx-auto float-lg-right text-center">
             <?php if(isset($info->rank->solo)) : ?>
-            <div class="player-rank-solo d-md-inline-block mx-lg-auto">
-              <div class="d-sm-inline-block align-top" data-toggle="tooltip" title="<?php echo $info->rank->solo->leagueName; ?> <p><?php echo $info->rank->solo->wins; ?>W <?php echo $info->rank->solo->losses; ?>L <?php echo round(($info->rank->solo->wins/(($info->rank->solo->wins)+($info->rank->solo->losses)))*100);?>%WR</p> ">
+            <div class="player-rank-solo d-md-inline-block px-md-3 px-lg-2 px-xl-4o">
+              <div class="d-sm-inline-block align-middle" data-toggle="tooltip" title="<?php echo $info->rank->solo->leagueName; ?><p class='m-0'><?php echo "WinRatio: " . round(($info->rank->solo->wins/(($info->rank->solo->wins)+($info->rank->solo->losses)))*100);?>%</p><p class='m-0'><?php echo $info->rank->solo->wins; ?>W <?php echo $info->rank->solo->losses; ?>L </p>">
                 <img class="player-rank-icon" src="assets/tier-icons/<?php echo strtolower($info->rank->solo->tier.'_'.$info->rank->solo->rank); ?>.png">
               </div>
-              <div class="player-rank-text d-lg-inline-block">
+              <div class="player-rank-text d-lg-inline-block align-middle">
                 <p class="p-queue text-muted"><small>SOLO/DUO</small></p>
                 <p class="p-rank lead"><?php echo $info->rank->solo->tier . " " . $info->rank->solo->rank ?></p>
                 <p class="p-lp text-muted"><small><?php echo $info->rank->solo->leaguePoints . "LP"; ?></small></p>
@@ -97,11 +106,11 @@
             </div>
             <?php endif; ?>
             <?php if(isset($info->rank->flex)) : ?>
-            <div class="player-rank-flex d-md-inline-block">
-              <div class="d-sm-inline-block align-top" data-toggle="tooltip" title="<?php echo $info->rank->flex->leagueName; ?> <p><?php echo $info->rank->flex->wins; ?>W <?php echo $info->rank->flex->losses; ?>L <?php echo round(($info->rank->flex->wins/(($info->rank->flex->wins)+($info->rank->flex->losses)))*100);?>%WR</p> ">
+            <div class="player-rank-flex d-md-inline-block px-md-3 px-lg-2 px-xl-4">
+              <div class="d-sm-inline-block align-middle" data-toggle="tooltip" title="<?php echo $info->rank->flex->leagueName; ?><p class='m-0'><?php echo "WinRatio: " . round(($info->rank->flex->wins/(($info->rank->flex->wins)+($info->rank->flex->losses)))*100);?>%</p><p class='m-0'><?php echo $info->rank->flex->wins; ?>W <?php echo $info->rank->flex->losses; ?>L </p>">
                 <img class="player-rank-icon" src="assets/tier-icons/<?php echo strtolower($info->rank->flex->tier.'_'.$info->rank->flex->rank); ?>.png">
               </div>
-              <div class="player-rank-text d-lg-inline-block">
+              <div class="player-rank-text d-lg-inline-block align-middle">
                 <p class="p-queue text-muted"><small>FLEX 5V5</small></p>
                 <p class="p-rank lead"><?php echo $info->rank->flex->tier . " " . $info->rank->flex->rank ?></p>
                 <p class="p-lp text-muted"><small><?php echo $info->rank->flex->leaguePoints . "LP"; ?></small></p>
